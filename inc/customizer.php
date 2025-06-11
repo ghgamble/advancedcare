e58c73583584d0a20307433879182afb84c360dd
Grace Gamble <gracegamble@Graces-Laptop.local>
2025-06-11 16:25:17 -0600

    added yeseva one font

--- inc/customizer.php
+++ inc/customizer.php
@@ -47,7 +47,8 @@ function advancedcare_customize_register($wp_customize) {
         "'Crimson Pro', serif" => 'Crimson Pro',
         "'Poppins', sans-serif" => 'Poppins',
         "'Great Vibes', cursive" => 'Great Vibes',
-        "'Playfair Display', serif" => 'Playfair Display'
+        "'Playfair Display', serif" => 'Playfair Display',
+        "'Yeseva One', serif" => 'Yeseva One'
     ];
 
     $wp_customize->add_setting('advancedcare_font_family', [
